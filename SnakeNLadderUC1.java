2a66d20e3c0680ba2ebfed1ffb04ee41b51f83e4
softramesh29 <softramesh29@gmail.com>
2022-04-17 23:44:09 +0530

    Game start with single player at zero position

---
+++ SnakeNLadderUC1.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.SnakeandLadder;
+
+public class SnakeNLadderUC1 {
+	public static void main(String[] args) {
+		int playerPosition = 0;
+		System.out.println("Player position is : "+playerPosition);
+	}
+ }
