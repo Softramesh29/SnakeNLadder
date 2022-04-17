b7ed66f215732ae1243dbce36ddd70db2208a622
softramesh29 <softramesh29@gmail.com>
2022-04-17 23:46:45 +0530

    Generating random number between 1 - 6

---
+++ SnakeNLadderUC2.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.SnakeandLadder;
+
+import java.util.Random;
+
+public class SnakeNLadderUC2 {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int roll = 1 + r.nextInt(6);
+		System.out.println(roll);
+	}
+}
+
+
+
